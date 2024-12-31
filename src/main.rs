a33444e2030ff9ec32b6eda47ba4ab24acbc820f
SAI KATTERISHETTY <skrao@SAIs-MacBook-Air.local>
2024-12-30 22:34:52 -0600

    Added more code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use IPScanner::{InputIp, IpAddressRange, IpPinger};
 
-
-
-
 fn main() {
     let result = InputIp::parse("192.168.86", 1, 255);
     let mut ip_addr_range = IpAddressRange::new();
     ip_addr_range.generate(&result);
-    println!("IP Addresses {}", ip_addr_range.ip_count());
+    println!("IP Addresses to scan {}", ip_addr_range.ip_count());
 
-    let ip_pinger = IpPinger::new(ip_addr_range.ip_addresses, 1, 1);
+    let ip_pinger = IpPinger::new(ip_addr_range.ip_addresses, 2, 1);
 
     let results = ip_pinger.ping_in_range();
 
